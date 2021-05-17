9b374dd31b5dab1e1b2b69afcca9d6cb16cf2c9a
Prashant Maroli <prashant.maroli@evolvussolutions.com>
2021-05-17 10:09:58 +0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     orchidRuntime("io.github.javaeden.orchid:OrchidTaxonomies:$orchid_version")
     orchidRuntime("io.github.javaeden.orchid:OrchidFutureImperfect:$orchid_version")
     orchidRuntime("io.github.javaeden.orchid:OrchidAsciidoc:$orchid_version")
+    orchidRuntime("io.github.javaeden.orchid:OrchidCopper:0.21.2")
 }
 
 tasks.withType<KotlinCompile> {
